3c2728cbfe54cf34f9d9346abdbe1ac38ec3a030
UbioZur <13650202+UbioZur@users.noreply.github.com>
2025-05-15 16:50:19 +0900

    Make sure header is not removed on merge

--- headless.sh
+++ headless.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-##  ~ UbioZur - https://github.com/UbioZur ~  ##
+# ##  ~ UbioZur - https://github.com/UbioZur ~  ##
 
 # Setup and bootstrap my minimal debian install
 # License: MIT | https://github.com/UbioZur/debian-setup/raw/main/LICENSE
