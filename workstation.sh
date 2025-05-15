4937278b1ec75d1935297f3a0add8a564e25bca4
UbioZur <13650202+UbioZur@users.noreply.github.com>
2025-05-15 18:14:15 +0900

    Trying to get the runner to work

--- workstation.sh
+++ workstation.sh
@@ -25,7 +25,7 @@ source ./tasks/tmpfolder.bash
 
 # Constants
 readonly SCRIPT_NAME=$(basename "$0")
-readonly SCRIPT_TAG_LINE="Setup Personal Debian Desktop System"
+readonly SCRIPT_TAG_LINE="Setup Personal Debian Workstation System"
 
 # Cleanup Function
 cleanup() {
